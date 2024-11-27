382476742a86b6b5af9bc97185ff6f75912990dc
DaniHere <danihere@optipvp.xyz>
2024-11-27 10:37:25 +0200

    n_div update

--- n_div.cpp
+++ n_div.cpp
@@ -2,8 +2,7 @@ int n_div(int a) {
     a = abs(a);
     short numar_div = 0;
     for(int i = 1; i <= a; i++) {
-        if(a%i) continue;
-        else numar_div++;
+        if(!(a%i)) numar_div++;
     }
     return numar_div;
 }
